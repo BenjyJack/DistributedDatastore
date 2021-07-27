50fce78efa8169631a7838c207038b21a241471a
Tuvia Goldstein <tjgoldst@mail.yu.edu>
2021-07-27 11:16:59 -0400

    Testing push

--- datastore/src/main/java/com/ds/datastore/RequestListener.java
+++ datastore/src/main/java/com/ds/datastore/RequestListener.java
@@ -2,10 +2,10 @@ package com.ds.datastore;
 
 import java.util.UUID;
 
-import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +15,13 @@ public class RequestListener implements ServletRequestListener{
     private Logger logger = LoggerFactory.getLogger(RequestListener.class);
     @Override
     public void requestInitialized(ServletRequestEvent sre){
-        ServletRequest request = sre.getServletRequest(); 
-        if(request.getAttribute("orderID") == null){
+        HttpServletRequest request = (HttpServletRequest)sre.getServletRequest();
+
+        if(request.getHeader("orderID") == null){
             request.setAttribute("orderID", String.valueOf(UUID.randomUUID()));
         }
+        logger.info("Call from {}", request.getHeader("referer"));
+        logger.info("Request Type: {}", request.getMethod());
         logger.info("Request {} received", request.getAttribute("orderID"));
     }
 }
